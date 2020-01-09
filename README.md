0ab2db171b89f5179de875dc1fbba451db5e8b30
罗亮 <luoliang1@staff.weibo.com>
2020-01-09 18:51:46 +0800

    Update.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # wechat-article-background
+
 解决微信文章MAC上查看黑色背景问题
---
+++ script.js
@@ -0,0 +1,8 @@
+$().ready(changeBackground);
+
+function changeBackground() {
+    $(".appmsg_skin_default .rich_media_area_primary").css("background", "#FFF")
+    $(".rich_media_content").css("background", "#FFF")
+    $(".rich_media_content").css("color", "#000")
+}
+
