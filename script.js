bfb73d377aa5d3ce3fd468b573b3b65563cd953f
罗亮 <luoliang1@staff.weibo.com>
2020-01-09 18:56:15 +0800

    Update.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 $().ready(changeBackground);
 
 function changeBackground() {
+    $("body").css("color", "#000")
+    $(".icon_appmsg_tag.appmsg_title_tag").css("color", "#000")
     $(".appmsg_skin_default .rich_media_area_primary").css("background", "#FFF")
     $(".rich_media_content").css("background", "#FFF")
     $(".rich_media_content").css("color", "#000")
+    $(".rich_media_meta_text").css("color", "#333")
 }
 
